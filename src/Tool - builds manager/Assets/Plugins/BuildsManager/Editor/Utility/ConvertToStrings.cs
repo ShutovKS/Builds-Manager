6b6115824fe5553c366577c8fa5abebb67aad946
ShutovKS <i@shutovks.ru>
2024-02-10 03:32:14 +0300

    Added parametr addon in ConvertToStrings

--- src/Tool - builds manager/Assets/Plugins/BuildsManager/Editor/Utility/ConvertToStrings.cs
+++ src/Tool - builds manager/Assets/Plugins/BuildsManager/Editor/Utility/ConvertToStrings.cs
@@ -18,6 +18,7 @@ namespace BuildsManager.Utility
             s = s.Replace("$MONTH", $"{usedDate.Date.Month}");
             s = s.Replace("$DAY", $"{usedDate.Date.Day}");
             s = s.Replace("$TIME", $"{usedDate.Hour}_{usedDate.Minute}");
+            s = s.Replace("$ADDONS", $"{AddonsUsed.GetAddonName(data.addonsUsed)}");
             s = s.Replace("$EXECUTABLE", GetBuildTargetExecutable(data.target));
             return s;
         }
@@ -34,6 +35,7 @@ namespace BuildsManager.Utility
             s = s.Replace("$MONTH", $"{usedDate.Date.Month}");
             s = s.Replace("$DAY", $"{usedDate.Date.Day}");
             s = s.Replace("$TIME", $"{usedDate.Hour}_{usedDate.Minute}");
+            s = s.Replace("$ADDONS", $"{AddonsUsed.GetAddonName(data.addonsUsed)}");
             s = s.Contains("$EXECUTABLE")
                 ? s.Replace("$EXECUTABLE", GetBuildTargetExecutable(data.target))
                 : s + ".zip";
