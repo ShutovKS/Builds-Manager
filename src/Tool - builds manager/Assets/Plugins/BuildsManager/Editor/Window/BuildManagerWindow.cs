971a18e38d574e2e154a846e2becede726bd330c
ShutovKS <schutow.kir@yandex.ru>
2024-03-03 15:02:42 +0300

    Build data looks like cards

--- src/Tool - builds manager/Assets/Plugins/BuildsManager/Editor/Window/BuildManagerWindow.cs
+++ src/Tool - builds manager/Assets/Plugins/BuildsManager/Editor/Window/BuildManagerWindow.cs
@@ -15,9 +15,8 @@ namespace BuildsManager.Window
         private static bool _globalDataFoldout = true;
         private static bool _buildsDataFoldout = true;
 
-        private static Vector2 _scrollPosSequence = Vector2.zero;
-        private Vector2 _scrollPositionBuilds = Vector2.zero;
-        
+        private static Vector2 _scrollPositionBuilds = Vector2.zero;
+
         [MenuItem("File/Builds Manager", false, 205)]
         public static void ShowWindow()
         {
@@ -32,58 +31,50 @@ namespace BuildsManager.Window
             {
                 MainManager.LoadSettings();
             }
+            
+            DrawGlobalBuildData();
+            
+            DrawBuildDataList();
 
+            DrawBuildsData();
+
+            DrawBuildButtons();
+            
+            EditorUtility.SetDirty(Settings);
+        }
+
+        #region Main Drawers
+
+        private static void DrawGlobalBuildData()
+        {
             _globalDataFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(_globalDataFoldout, "Глобальные данные");
 
             if (_globalDataFoldout)
             {
                 ++EditorGUI.indentLevel;
 
-                DrawGlobalBuildData();
+                PlayerSettings.companyName = EditorGUILayout.TextField("Название компании", PlayerSettings.companyName);
+                PlayerSettings.productName = EditorGUILayout.TextField("Название проекта", PlayerSettings.productName);
+                PlayerSettings.bundleVersion = EditorGUILayout.TextField("Версия", PlayerSettings.bundleVersion);
+
+                EditorGUILayout.Space(5);
+
+                EditorGUILayout.LabelField("Для Android");
+                PlayerSettings.Android.bundleVersionCode = EditorGUILayout.IntField("Версия пакета Android",
+                    PlayerSettings.Android.bundleVersionCode);
+                PlayerSettings.Android.keystorePass =
+                    EditorGUILayout.TextField("Android keystore pass", PlayerSettings.Android.keystorePass);
+                PlayerSettings.Android.keyaliasPass =
+                    EditorGUILayout.TextField("Android keyalias pass", PlayerSettings.Android.keyaliasPass);
 
                 --EditorGUI.indentLevel;
             }
 
             EditorGUILayout.EndFoldoutHeaderGroup();
-            
-            GUILayout.Label("Build Data List", EditorStyles.boldLabel);
+        }
 
-            _scrollPositionBuilds = EditorGUILayout.BeginScrollView(_scrollPositionBuilds);
-
-            if (Settings != null && Settings.builds != null)
-            {
-                for (var i = 0; i < Settings.builds.Count; i++)
-                {
-                    EditorGUILayout.BeginVertical(EditorStyles.helpBox);
-
-                    EditorGUILayout.LabelField($"Build {i + 1}", EditorStyles.boldLabel);
-
-                    Settings.builds[i].isEnabled = EditorGUILayout.Toggle("Enabled", Settings.builds[i].isEnabled);
-                    Settings.builds[i].isCompress = EditorGUILayout.Toggle("Compress", Settings.builds[i].isCompress);
-                    Settings.builds[i].buildPath = EditorGUILayout.TextField("Build Path", Settings.builds[i].buildPath);
-                    Settings.builds[i].options = (BuildOptions)EditorGUILayout.EnumPopup("Build Options", Settings.builds[i].options);
-                    Settings.builds[i].target = (BuildTarget)EditorGUILayout.EnumPopup("Build Target", Settings.builds[i].target);
-                    Settings.builds[i].targetGroup = (BuildTargetGroup)EditorGUILayout.EnumPopup("Target Group", Settings.builds[i].targetGroup);
-
-                    // Можно добавить пользовательский интерфейс для настройки AddonsUsed
-                    
-                    if (GUILayout.Button("Remove", GUILayout.Width(80)))
-                    {
-                        Settings.builds.RemoveAt(i);
-                        break;
-                    }
-
-                    EditorGUILayout.EndVertical();
-                }
-            }
-
-            EditorGUILayout.EndScrollView();
-
-            if (GUILayout.Button("Add Build"))
-            {
-                Settings.builds?.Add(new BuildData());
-            }
-            
+        private static void DrawBuildsData()
+        {
             _buildsDataFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(_buildsDataFoldout, "Данные билда");
 
             if (_buildsDataFoldout)
@@ -98,31 +89,71 @@ namespace BuildsManager.Window
             }
 
             EditorGUILayout.EndFoldoutHeaderGroup();
+            
+            return;
+            
+            static void DrawConfiguredBuilds()
+            {
+                EditorGUILayout.BeginVertical();
 
-            DrawBuildButtons();
+                Settings.generalScriptingDefineSymbols = EditorGUILayout.TextField("Scripting Define Symbols Default",
+                    Settings.generalScriptingDefineSymbols);
+                Settings.isReleaseBuild = EditorGUILayout.Toggle("Is Release build", Settings.isReleaseBuild);
 
-            EditorGUILayout.Space(5f);
+                EditorGUILayout.EndVertical();
+            }
 
-            EditorUtility.SetDirty(Settings);
+            static void DrawPathData()
+            {
+                Settings.outputRoot = EditorGUILayout.TextField("Output root", Settings.outputRoot);
+                Settings.middlePath = EditorGUILayout.TextField("Middle path", Settings.middlePath);
+                Settings.dirPathForPostProcess =
+                    EditorGUILayout.TextField("Dir path for process", Settings.dirPathForPostProcess);
+            }
         }
 
-        #region Main Drawers
-
-        private static void DrawGlobalBuildData()
+        private static void DrawBuildDataList()
         {
-            PlayerSettings.companyName = EditorGUILayout.TextField("Название компании", PlayerSettings.companyName);
-            PlayerSettings.productName = EditorGUILayout.TextField("Название проекта", PlayerSettings.productName);
-            PlayerSettings.bundleVersion = EditorGUILayout.TextField("Версия", PlayerSettings.bundleVersion);
+            GUILayout.Label("Build Data List", EditorStyles.boldLabel);
 
-            EditorGUILayout.Space(5);
+            _scrollPositionBuilds = EditorGUILayout.BeginScrollView(_scrollPositionBuilds);
+            EditorGUILayout.BeginHorizontal(GUILayout.Width(Settings.builds.Count * 250));
 
-            EditorGUILayout.LabelField("Для Android");
-            PlayerSettings.Android.bundleVersionCode = EditorGUILayout.IntField("Версия пакета Android",
-                PlayerSettings.Android.bundleVersionCode);
-            PlayerSettings.Android.keystorePass =
-                EditorGUILayout.TextField("Android keystore pass", PlayerSettings.Android.keystorePass);
-            PlayerSettings.Android.keyaliasPass =
-                EditorGUILayout.TextField("Android keyalias pass", PlayerSettings.Android.keyaliasPass);
+            if (Settings != null && Settings.builds != null)
+            {
+                for (var i = 0; i < Settings.builds.Count; i++)
+                {
+                    var settingsBuild = Settings.builds[i];
+                    
+                    EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                    EditorGUILayout.LabelField($"Build {settingsBuild.target}", EditorStyles.boldLabel);
+
+                    settingsBuild.isEnabled = EditorGUILayout.Toggle("Enabled", settingsBuild.isEnabled);
+                    settingsBuild.isCompress = EditorGUILayout.Toggle("Compress", settingsBuild.isCompress);
+                    settingsBuild.buildPath = EditorGUILayout.TextField("Build Path", settingsBuild.buildPath);
+                    settingsBuild.options = (BuildOptions)EditorGUILayout.EnumFlagsField("Build Options", settingsBuild.options);
+                    settingsBuild.target = (BuildTarget)EditorGUILayout.EnumPopup("Build Target", settingsBuild.target);
+                    settingsBuild.targetGroup = (BuildTargetGroup)EditorGUILayout.EnumPopup("Target Group", settingsBuild.targetGroup);
+
+                    // Можно добавить пользовательский интерфейс для настройки AddonsUsed
+
+                    if (GUILayout.Button("Remove", GUILayout.Width(80)))
+                    {
+                        Settings.builds.RemoveAt(i);
+                        break;
+                    }
+
+                    EditorGUILayout.EndVertical();
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndScrollView();
+
+            if (GUILayout.Button("Add Build"))
+            {
+                Settings.builds?.Add(new BuildData());
+            }
         }
 
         private static void DrawBuildButtons()
@@ -145,29 +176,12 @@ namespace BuildsManager.Window
             }
 
             EditorGUILayout.EndHorizontal();
-
+            
             GUI.backgroundColor = prevColor;
+            
+            EditorGUILayout.Space(5f);
         }
-
-        private static void DrawConfiguredBuilds()
-        {
-            EditorGUILayout.BeginVertical();
-
-            Settings.generalScriptingDefineSymbols = EditorGUILayout.TextField("Scripting Define Symbols Default",
-                Settings.generalScriptingDefineSymbols);
-            Settings.isReleaseBuild = EditorGUILayout.Toggle("Is Release build", Settings.isReleaseBuild);
-
-            EditorGUILayout.EndVertical();
-        }
-
-        private static void DrawPathData()
-        {
-            Settings.outputRoot = EditorGUILayout.TextField("Output root", Settings.outputRoot);
-            Settings.middlePath = EditorGUILayout.TextField("Middle path", Settings.middlePath);
-            Settings.dirPathForPostProcess =
-                EditorGUILayout.TextField("Dir path for process", Settings.dirPathForPostProcess);
-        }
-
+        
         #endregion
 
         #region Callbacks
